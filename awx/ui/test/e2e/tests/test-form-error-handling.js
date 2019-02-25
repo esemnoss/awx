f7c73fb5e1c28816046868da8053fc2015613b17
Daniel Sami <dsami@redhat.com>
2019-02-25 15:59:24 -0500

    New tests for form validation

--- awx/ui/test/e2e/tests/test-form-error-handling.js
+++ awx/ui/test/e2e/tests/test-form-error-handling.js
@@ -12,12 +12,13 @@ const templatesNavTab = "//at-side-nav-item[contains(@name, 'TEMPLATES')]";
 module.exports = {
     before: (client, done) => {
         const resources = [
-            getJobTemplate(namespace)
+            getJobTemplate(namespace),
+            getJobTemplate(namespace + '-2')
         ];
 
         Promise.all(resources)
-            .then(([jt]) => {
-                data = { jt };
+            .then(([jt, jt2]) => {
+                data = { jt, jt2 };
                 done();
             });
         client
@@ -50,15 +51,52 @@ module.exports = {
             '//div[@id="job_template-name-api-error"]').to.be.visible.before(3000);
     },
 
-    'Test incorrect format when creating a job template': client => {},
+    'Test duplicate template name handling when creating a job template': client => {
+        client
+            .waitForElementNotVisible('//*[@id="alert_ok_btn"]')
+            .waitForElementVisible('//div[contains(@class, "Form-title")]')
+            .clearValue('//input[@id="job_template_name"]')
+            .setValue('//input[@id="job_template_name"]',
+                ['test-form-error-handling-job-template', client.Keys.ENTER])
+            .findThenClick('//*[@id="job_template_save_btn"]')
+            .findThenClick('//*[@id="alert_ok_btn"]');
+    },
 
-    'Test max character limit when editing a job template': client => {},
+    'Test incorrect format when creating a job template': client => {
+        // clear values after entering and look for error
+        // ensure not saveable
+    },
+
+    'Test max character limit when editing a job template': client => {
+        client
+            .waitForElementNotVisible('//*[@id="alert_ok_btn"]')
+            .waitForElementVisible('//a[text()="test-form-error-handling-job-template"]')
+            .findThenClick('.//a[text()="test-form-error-handling-job-template"]')
+            .waitForElementVisible('//div[contains(@class, "Form-title") and text()="test-form-error-handling-job-template"]')
+            .clearValue('//input[@id="job_template_name"]')
+            .setValue('//input[@id="job_template_name"]',
+                ['a'.repeat(513), client.Keys.ENTER])
+            .findThenClick('//*[@id="job_template_save_btn"]')
+            .findThenClick('//*[@id="alert_ok_btn"]');
+
+        client.expect.element('//div[@id="job_template_name_group"]' +
+            '//div[@id="job_template-name-api-error"]').to.be.visible.before(3000);
+   },
 
     'Test incorrect format when editing a job template': client => {},
 
-    'Test duplicate template name handling when creating a job template': client => {},
-
-    'Test duplicate template name handling when editing a job template': client => {},
+    'Test duplicate template name handling when editing a job template': client => {
+        client
+            .waitForElementNotVisible('//*[@id="alert_ok_btn"]')
+            .waitForElementVisible('//a[text()="test-form-error-handling-2-job-template"]')
+            .findThenClick('.//a[text()="test-form-error-handling-2-job-template"]')
+            .waitForElementVisible('//div[contains(@class, "Form-title") and text()="test-form-error-handling-2-job-template"]')
+            .clearValue('//input[@id="job_template_name"]')
+            .setValue('//input[@id="job_template_name"]',
+                ['test-form-error-handling-job-template', client.Keys.ENTER])
+            .findThenClick('//*[@id="job_template_save_btn"]')
+            .findThenClick('//*[@id="alert_ok_btn"]');
+    },
 
     'Test max character limit when creating an organization': client => {},
 
